90d1d36fb53f38341171a47c8464712ea3abc0a8
butirpadi <butirpadi@gmail.com>
2017-12-23 14:06:03 +0700

    [UPDATE] Index Report Pengiriman, hilangkan tab Group Report

--- resources/views/report/pengiriman/index.blade.php
+++ resources/views/report/pengiriman/index.blade.php
@@ -73,7 +73,7 @@
             <div class="nav-tabs-custom">
                 <ul class="nav nav-tabs">
                   <li class="active"><a href="#tab_1" data-toggle="tab">Basic</a></li>
-                  <li><a href="#tab_2" data-toggle="tab">Group Report</a></li>
+                  <!-- <li><a href="#tab_2" data-toggle="tab">Group Report</a></li> -->
                 </ul>
                 <div class="tab-content">
                   <div class="tab-pane active" id="tab_1">
@@ -120,37 +120,7 @@
                         </div>
                     </form>
                   </div>
-                  <div class="tab-pane" id="tab_2">
-                      <!-- GROUP REPORT -->
-                      <form method="POST" action="report/pengiriman/group-report" target="_blank">
-                        <div class="row" >
-                            <div class="col-xs-6" >
-                                <div class="form-group">
-                                    <label >Tanggal</label>
-                                    <div class='input-group' style="width: 100%;" >
-                                        <input type="text" name="tanggal_awal" class="input-tanggal form-control" value="{{date('d-m-Y')}}" required>
-                                        <div class='input-group-field' style="padding-left: 5px;" >
-                                            <input  type="text" name="tanggal_akhir" class="input-tanggal form-control" value="{{date('d-m-Y')}}" required>
-
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <label >customer</label>
-                                    {!! Form::select('customer',$select_customer,null,['class'=>'form-control select2']) !!}
-                                </div>
-                            </div>
-                            <div class="col-xs-6" >
-                                
-                            </div> 
-                            <div class="col-xs-12" >
-                                <button type="submit" class="btn btn-primary" id="btn-save" ><i class="fa fa-check" ></i> Submit</button>
-                                <a class="btn btn-danger" id="btn-cancel-save" href="pengiriman" ><i class="fa fa-close" ></i> Close</a>
-                            </div>
-                        </div>
-                    </form>
-                  </div>
-                </div>
+                  
             </div>
         </div>
     </div>
